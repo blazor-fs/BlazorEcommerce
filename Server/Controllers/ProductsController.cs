eb96e5ef4255c4cb1e6295618fc2677a17e24299
Filipe Silva <filipeqs@outlook.com>
2023-11-07 11:20:28 -0500

    add swagger

--- Server/Controllers/ProductsController.cs
+++ Server/Controllers/ProductsController.cs
@@ -35,7 +35,7 @@ public class ProductsController : ControllerBase
     };
 
     [HttpGet]
-    public async Task<IActionResult> GetProducts()
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
         return Ok(_products);
     }
